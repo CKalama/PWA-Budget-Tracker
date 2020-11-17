ba16a13343c43a25ca6ae47d7141afdfdc417576
Chris Kalama <chriskalama96@gmail.com>
2020-11-16 18:05:41 -0600

    With Tom, was bringing over files for service-worker.js and db.js from previous activites.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+//const db = require('./models')
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
